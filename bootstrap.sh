4a6c78cb814739dbfb3e29962750ea956d0d7071
James Culveyhouse <jculveyhouse@gmail.com>
2018-04-29 12:51:17 -0700

    Adding fzf to bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,11 @@
+# Install fzf
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
+
+# Vim plug
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
 # Grab the color scheme
 mkdir -p ~/.vim/colors
 cd ~/.vim/colors
